04a3f10916c0855de4c11a421b3ae910ec3c84fa
cfgarciaq <cfgarciaq@gmail.com>
2023-06-29 22:37:56 +0200

    Update FormatSpecifiers.c

--- C/src/FormatSpecifiers.c
+++ C/src/FormatSpecifiers.c
@@ -27,10 +27,14 @@ int main(){
     printf("Item 1: $%.2f\n",item1);
     printf("Item 3: $%.2f\n",item2);
     printf("Item 2: $%.2f\n",item3);
-    printf("============");
+    printf("============\n");
     printf("Item 1: $%8.2f\n",item1);
     printf("Item 3: $%8.2f\n",item2);
     printf("Item 2: $%8.2f\n",item3);
+    printf("============\n");
+    printf("Item 1: $%-8.2f\n",item1);
+    printf("Item 3: $%-8.2f\n",item2);
+    printf("Item 2: $%-8.2f\n",item3);
 
     return 0;
 }
