9a7c23350acb2cdb2327d887fb1d9768c68de7c1
cfgarciaq <cfgarciaq@gmail.com>
2023-06-30 01:35:18 +0200

    Update TemperatureConvertion.c

--- C/src/TemperatureConvertion.c
+++ C/src/TemperatureConvertion.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+
 
 
 int main(){
 
     char unit;
     float temp;
+    bool isUnitCorrect = false;
 
-    printf("\n Is the temperature in (F) or in (C)?: ");
-    scanf("%c",&unit);
+    while(!isUnitCorrect){
 
-    unit = toupper(unit); //transforany lowercase to upercase
+        printf("\nIs the temperature in (F) or in (C)?: ");
+        scanf("%c",&unit);
 
-    if(unit == 'C'){
-        printf("The temp is currently in C");
+        if(unit == 'c' || unit == 'f'){
+            unit = toupper(unit); //changes any lowercase to upercase            
+        }
+
+        if(unit == 'C'){
+            printf("\nThe temp is currently in C");
+            printf("\nEnter the temp value:");
+            scanf("%f",&temp);
+
+            temp = (temp * 9 / 5) + 32;
+            printf("\nThe temp in Farenheit is: %.1f", temp);
+
+            isUnitCorrect = true;
+        }
+        else if(unit == 'F'){
+            printf("\nThe temp is currently in F");
+            printf("\nEnter the temp value:");   
+            scanf("%f",&temp);
+            
+            temp = ((temp - 32) * 5) / 9;
+            printf("\nThe temp in Celsius is: %.1f", temp);
+
+            isUnitCorrect = true;
+        }
+        else{
+            printf("\'%c\' is not a valid unit, only F or C", unit);
+            printf("\nTry again\n");
+        }
     }
-    else if(unit == 'F'){
-        printf("The temp is currently in F");
-    }
-    else{
-        printf("%c is not a valid unit, only F or C", unit);
-    }
+
 
     return 0;
 }
