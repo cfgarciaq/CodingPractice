462e493d6fa3737c07ad0bd1b1af5b1e23c7e8d4
cfgarciaq <cfgarciaq@gmail.com>
2023-06-29 21:06:48 +0200

    Update CirclePerimeter.c

--- C/src/CirclePerimeter.c
+++ C/src/CirclePerimeter.c
@@ -5,13 +5,19 @@ int main(){
     const double PI = 3.14169;
     double radius;
     double circumference;
+    double area;
 
     printf("Enter radious of a circle: ");
     scanf("%lf", &radius);
 
     circumference = 2*PI*radius;
 
+    area = PI*radius*radius;
+
     printf("circumference: %lf", circumference);
+    printf("area: %lf", area);
+
     
+
     return 0;
 }
