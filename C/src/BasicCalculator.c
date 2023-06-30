6545c5d162fc0da149f072e126fd618830e75817
cfgarciaq <cfgarciaq@gmail.com>
2023-06-30 02:07:12 +0200

    Create BasicCalculator.c

---
+++ C/src/BasicCalculator.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    bool isCorrect = false;
+
+    while(!isCorrect){
+        
+        printf("\nEnter an operator (+ - * /): ");
+        scanf("%c", &operator);
+
+        if(operator == '+' || operator == '-' || operator == '*' || operator == '/'){
+            isCorrect = true;
+        }
+    }
+
+
+    printf("Enter number 1: ");
+    scanf("%lf", &num1);
+    
+    printf("Enter number 2: ");
+    scanf("%lf", &num2);
+
+    switch (operator)
+    {
+    case '+':
+        result = num1 + num2;
+        printf("\nResult: %.2lf", result);
+        break;
+    case '-':
+        result = num1 - num2;
+        printf("\nResult: %.2lf", result);
+        break;
+    case '*':
+        result = num1 * num2;
+        printf("\nResult: %.2lf", result);
+        break;
+    case '/':
+        result = num1 / num2;
+        printf("\nResult: %.2lf", result);
+        break;    
+    default:
+        break;
+    }
+
+
+}
